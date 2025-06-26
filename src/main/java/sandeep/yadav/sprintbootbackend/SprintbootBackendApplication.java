31048d19a77d2bb828a0972b1e81a020eab52d64
Samrat101 <samratrao101@gmail.com>
2025-06-27 05:19:34 +0530

    Initial commit

---
+++ src/main/java/sandeep/yadav/sprintbootbackend/SprintbootBackendApplication.java
@@ -0,0 +1,13 @@
+package sandeep.yadav.sprintbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/sandeep/yadav/sprintbootbackend/model/Employee.java
@@ -0,0 +1,20 @@
+package sandeep.yadav.sprintbootbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email",nullable = false)
+    private String email;
+}
